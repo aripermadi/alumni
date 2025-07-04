5c002f301d258c53fff910c8b5eedf846cac3bd0
aripermadi <arisuneo@gmail.com>
2025-07-04 20:54:03 +0700

    Menambahkan penanganan fallback untuk permintaan gambar di service worker. Jika permintaan gambar gagal, akan mengembalikan gambar placeholder sebagai alternatif. Ini meningkatkan pengalaman pengguna dengan memastikan tampilan yang konsisten saat gambar tidak dapat dimuat.

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,7 +10,12 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        // return fallback image jika request gambar gagal
+        if (event.request.destination === 'image') {
+          return caches.match('/images/placeholder-profile.png');
+        }
+      });
     })
   );
 }); 
